306d56fea27ac56e312abfcf4da013a809bb8bc6
Ethan Yang <ethan.yang@messagexchange.com>
2014-11-03 14:56:15 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,17 @@
 # This script has not been finished. Please do not use it.
 
 #!/bin/bash
+
+# Names of the files to link
 filename_list=(vim vimrc zshrc bash_aliases rvmrc)
+
 for filename in "${filename_list[@]}"; do
   origin="~/dotfiles/$filename"
   destination="~/.$filename"
 
-  echo "Linking $origin to $destination..."
+  echo "Linking '$origin' to '$destination'..."
+
   ln -s $origin $destination
+
   echo "Done!\n"
 done
